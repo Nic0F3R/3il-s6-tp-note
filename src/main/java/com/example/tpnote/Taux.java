84de3424e703d61dba497e5049b80df5cbf8d27a
NicolasIUT <ferrayen@3il.fr>
2023-03-31 15:22:03 +0200

    feat: Création des attributs, du constructeur et des getters/setters de Taux

---
+++ src/main/java/com/example/tpnote/Taux.java
@@ -0,0 +1,52 @@
+package com.example.tpnote;
+
+/**
+ * Classe Taux (modèle du MVC) permettant de gérer les taux sur une transaction bancaire
+ *
+ * @since le 31/03/2023
+ * @version le 31/03/2023
+ *
+ * @author Nicolas Ferrayé - 3iL
+ */
+public class Taux {
+
+    private String nom;
+    private double seuilInferieur;
+    private double seuilSuperieur;
+
+    /**
+     * Constructeur de Taux
+     *
+     * @param pfNom le nom du Taux
+     * @param pfSeuilInferieur seuil inférieur du Taux
+     * @param pfSeuilSuperieur seuil supérieur du Taux
+     */
+    public Taux(String pfNom, double pfSeuilInferieur, double pfSeuilSuperieur) throws Exception {
+
+    }
+
+    public String getNom() {
+        return "";
+    }
+
+    public double getSeuilInferieur() {
+        return 0.0;
+    }
+
+    public double getSeuilSuperieur() {
+        return 0.0;
+    }
+
+    public void setNom(String pfNom) {
+
+    }
+
+    public void setSeuilInferieur(double pfSeuilInferieur) {
+
+    }
+
+    public void setSeuilSuperieur(double pfSeuilSuperieur) {
+
+    }
+
+}
