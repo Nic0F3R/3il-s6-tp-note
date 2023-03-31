bcbda8a31e480213ff33fee18cdd1154de2d9347
NicolasIUT <ferrayen@3il.fr>
2023-03-31 15:25:12 +0200

    feat: Création d'un test qui échoue pour le constructeur de Taux

---
+++ src/test/java/com/example/tpnote/TauxTest.java
@@ -0,0 +1,26 @@
+package com.example.tpnote;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TauxTest {
+
+    // ---- TESTS CONSTRUCTEUR ---- //
+    @Test
+    void testConstructeur1() {
+        try {
+            Taux t1 = new Taux("Nom", 0, 1000);
+            Taux t2 = new Taux("Nom", 1000, 2000);
+        } catch(Exception e) {
+            System.err.println("Erreur testConstructeur1() de TauxTest");
+        }
+    }
+
+    @Test
+    void testConstructeur2() {
+        assertThrows(Exception.class, () -> {
+            Taux t1 = new Taux("", 0, 1000);
+        });
+    }
+}
