3143cacf476f3dd1bb89d82f17d83c8ab1425f4e
NicolasIUT <ferrayen@3il.fr>
2023-03-31 16:04:37 +0200

    feat: création de la fonction et ajout des attributs permettant l'import des Taux depuis taux.txt

--- src/main/java/com/example/tpnote/GestionnaireBanque.java
+++ src/main/java/com/example/tpnote/GestionnaireBanque.java
@@ -6,7 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Modèle de l'application GestionnaireBanque
@@ -17,6 +22,12 @@ import java.io.IOException;
  * @author Nicolas Ferrayé - 3iL
  */
 public class GestionnaireBanque extends Application {
+
+    private List<Transaction> al_transactions;
+    private List<Taux> al_taux = new ArrayList<Taux>();
+
+    private double solde;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GestionnaireBanque.class.getResource("IHM.fxml"));
@@ -26,9 +37,65 @@ public class GestionnaireBanque extends Application {
         stage.getIcons().add(new Image("file:Images/logo3iL.png"));
         stage.setResizable(false);
         stage.show();
+
+        try {
+            loadTaux();
+        } catch(Exception e) {
+            System.err.println("Erreur lors du chargement des Taux");
+        }
     }
 
     public static void main(String[] args) {
         launch();
+    }
+
+    /**
+     * Permet d'initialiser la liste des Taux à partir du fichier taux.txt
+     */
+    private void loadTaux() throws Exception {
+
+        try {
+
+            // Déclaration des variables de traitement
+            String ligne;
+            String[] tabLigne;
+            String nomTaux;
+            double seuilInferieur;
+            double seuilSuperieur;
+            Taux taux;
+
+            // Déclaration et initialisation de la variable file et de son Scanner
+            File file = new File("Data", "taux.txt");
+
+            Scanner sc = new Scanner(file);
+
+            // Parcours ligne par ligne du fichier taux.txt
+            while(sc.hasNextLine()) {
+
+                ligne = sc.nextLine();
+
+                // Séparation des données avec le séparateur " "
+                tabLigne = ligne.split(" ");
+
+                nomTaux = tabLigne[0];
+                seuilInferieur = Double.parseDouble(tabLigne[1]);
+                seuilSuperieur = Double.parseDouble(tabLigne[2]);
+
+                // Initialisation de l'objet taux avec les valeurs de la ligne en cours
+                taux = new Taux(nomTaux, seuilInferieur, seuilSuperieur);
+
+                // Ajout de l'objet taux à l'ArrayList al_taux
+                this.al_taux.add(taux);
+
+                System.out.println("Nom : " + nomTaux + " ; " + "seuil i : " + seuilInferieur + " ; " + "seuil s : " + seuilSuperieur + "\n");
+
+            }
+
+
+
+
+        } catch(Exception e) {
+            System.err.println("Erreur sur le chargement des Taux dans l'application");
+        }
     }
 }
